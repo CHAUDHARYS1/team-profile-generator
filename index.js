14a799056ae425bedb4f31ae470109589d08ab87
Shital Chaudhary <shitalchau10@gmail.com>
2021-08-25 13:44:17 -0500

    created basic structure and made sure the correct index.html is generated

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-// const fs = require('fs');
-// const inquirer = require('inquirer');
-// const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateProfiles = require('./utils/generateProfiles');
 
+const questions = () => {
+    return inquirer.prompt([{
+        type: 'input',
+        name: 'projectTitle',
+        message: 'What is your project title?'
+    }]);
+};
+
+
+// TODO: Create a function to write index file
+function writeToFile(data) {
+    fs.writeFile('./dist/index.html', generateProfiles(data), err => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Page created! Checkout out index.html in this directory to see it!');
+
+    });
+};
+
+// TODO: Create a function to initialize app
+function init() {
+    questions().then((data) => {
+        writeToFile(data);
+        // console.log(data);
+    });
+};
+
+// Function call to initialize app
+init();
---
+++ utils/generateProfiles.js
@@ -0,0 +1,8 @@
+// TODO: Create a function to generate index.html
+function generateProfiles(data) {
+    return `
+    Hi
+  `;
+  }
+  
+  module.exports = generateProfiles;
