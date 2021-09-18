76fcec03fffa3435894a9bd36696cbdb0804b5a9
Shital Chaudhary <shitalchau10@gmail.com>
2021-09-17 21:41:11 -0500

    add employee class with test success

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,8 @@
-// jest tests
+const Employee = require('../lib/Employee');
+
+test("checking employee's data", () => {
+    let employee = new Employee('Shital', 1, 'shitalchau19@gmail.com')
+    expect(employee.getName()).toBe("Shital");
+    expect(employee.getRole()).toBe("Employee");
+    expect(employee.getEmail()).toBe("shitalchau19@gmail.com");
+});
